69d8dba3b5389c4dee12fc14469e51467e521331
gech11 <getachewfikadu77@gmail.com>
2022-02-24 01:17:37 -0800

    home layout implemented

---
+++ resources/js/components/navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-5 fs-5 flex space-x-8">
+    <p class="mx-3" :class="[currentUrl==homeLink? 'border-b-4 border-primary':'']"><a href="/">HOME</a></p>
+    <p class="mx-3" :class="[currentUrl==projectLink? 'border-b-4 border-primary':'']"><a href="/project/list">PROJECTS</a></p>
+    <p class="mx-3" :class="[currentUrl==blogLink? 'border-b-4 border-primary':'']"><a href="/blog/list">BLOGS</a></p>
+    <p class="mx-3" :class="[currentUrl==aboutLink? 'border-b-4 border-primary':'']"><a href="/about">ABOUT</a></p>
+    <p class="mx-3" :class="[currentUrl==contactLink? 'border-b-4 border-primary':'']"><a href="/contact">CONTACT US</a></p>
+    <p class="mx-3"><a href="/login">LOGIN</a></p>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      currentUrl:'',
+      homeLink:'http://127.0.0.1:8000/',
+      projectLink:'http://127.0.0.1:8000/project/list',
+      blogLink:'http://127.0.0.1:8000/blog/list',
+      aboutLink:'http://127.0.0.1:8000/about',
+      contactLink:'http://127.0.0.1:8000/contact',
+    }
+  },
+  mounted(){
+    this.currentUrl=window.location.href;
+  }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './resources/**/*.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+       "realstate": "url('/storage/realstate.jpg')"
+      }
+    },
   },
   plugins: [],
 }
